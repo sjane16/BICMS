ca6c1a86124cb9ade67e5a04db2147c8fa16752a
Samantha <samquibral16@gmail.com>
2025-09-22 15:02:40 -0800

    First commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <div className="header">
+            <img></img>
+            <p>Barangay Issuance and Complaint Management System</p>
+            <div className="b">
+            <button className="b1">Log-in</button>
+            <button className="b2">Register</button>
+            </div>
+        </div>
+    );
+}
+export default Header
